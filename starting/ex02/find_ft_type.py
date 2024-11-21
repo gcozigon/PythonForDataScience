ed8c1cc2a12c8219bc05119c0358281ea9de86d0
gcozigon <123009889+gcozigon@users.noreply.github.com>
2024-11-21 15:19:48 +0100

    delete starting

--- starting/ex02/find_ft_type.py
+++
@@ -1,6 +0,0 @@
-def all_thing_is_obj(object: any) -> int :
-    if (type(object).__name__ == "NoneType") :
-         
-    else : 
-        print(f'{type(object).__name__.capitalize()} : {type(object)}')
-    return 42; 
