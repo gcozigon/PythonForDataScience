6e09febba5f11c49995d7d8bb40caababfc3bbde
gcozigon <cozigon.gabriel@protonmail.com>
2024-11-27 18:10:51 +0100

    DATATABLE without the last ex

--- DataTable/ex02/aff_pop.py
+++ DataTable/ex02/aff_pop.py
@@ -1,23 +1,40 @@
+import pandas as pd
 import matplotlib.pyplot as plt
-from load_csv import load
 
-def main():
-    loads = load("life_expectancy_years.csv")
-    france_d = loads[loads['country'] == 'France']
-    print(france_d)
-    years = france_d.columns[1:]
-    life_expectancy = france_d.values[0][1:]
+def convert_to_million(value):
+    if 'K' in value:
+        return float(value.replace('K', '')) / 1000
+    elif 'M' in value:
+        return float(value.replace('M', ''))
+    else:
+        return float(value) / 1000000 
 
-    plt.plot(years, life_expectancy, label='France')
-    plt.title('Life Expectancy in France Over the Years')
-    plt.xlabel('Year')
-    plt.xticks(years[::40], rotation=45)
-    plt.ylabel('Life Expectancy')
-    plt.yticks(range(30, 101, 10))
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
+loads = pd.read_csv("population_total.csv")
+
+france_d = loads[loads['country'] == 'France']
+angola_d = loads[loads['country'] == 'Angola']
+
+years = france_d.columns[1:]
+france_population = france_d.values[0][1:]
+angola_population = angola_d.values[0][1:]
+
+france_population = [convert_to_million(value) for value in france_population]
+angola_population = [convert_to_million(value) for value in angola_population]
+
+years = [year for year in years if int(year) <= 2050]
+france_population = france_population[:len(years)]
+angola_population = angola_population[:len(years)]
+
+plt.figure(figsize=(10, 6))
+
+plt.plot(years, france_population, label="France", color='blue')
+plt.plot(years, angola_population, label="Angola", color='red')
+
+plt.title("Total Population Over Time")
+plt.xlabel("Year")
+plt.xticks(years[::40], rotation=45)
+plt.ylabel("Total Population (Millions)")
+plt.legend()
 
 
-if __name__ == "__main__":
-    main()
+plt.show()
